a713f51f3bc8596386a2ff7318ca856fcab06120
Kimura Tomoki <krmmmmmmrk@gmail.com>
2026-02-19 09:31:47 +0900

    refactor: 駅誰コンポーネントと関連記述を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.sass'
 import type { ReactElement } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LiquidDrip from './components/LiquidDrip'
+
 
 function App(): ReactElement {
   return (
@@ -9,8 +9,6 @@ function App(): ReactElement {
       <main>
         <Routes>
           <Route path="/" element={<h1>App</h1>} />
-          {/* 液体垂れエフェクトページ */}
-          <Route path="/liquid" element={<LiquidDrip />} />
         </Routes>
       </main>
     </BrowserRouter>
